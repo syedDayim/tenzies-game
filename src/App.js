103bea6bd8fe1e3e6effd5068023779e1042f7a0
Syed Dayim Shah <dayim1277@gmail.com>
2023-03-18 19:56:09 +0530

    Added Roll dice button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
+import { useState } from "react";
 import Die from "./Die.js";
 
 function App() {
+  const [randomNumbers, setRandomNumbers] = useState(allNewDice)
+
+  function allNewDice(){
+    const newDie = []
+
+    for(let i = 0; i < 10; i++){
+      newDie.push(Math.floor(Math.random() * 6))
+    }
+
+    return newDie
+    
+  }
+  function handleClick(){
+    setRandomNumbers(allNewDice())
+  }
+  const diceElement = randomNumbers.map((item)=>{
+    return(
+      <Die value={item}/>
+    )
+  })
+
+
   return (
     <main>
       <div className="dice-container">
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-                <Die value="1" />
-            </div>
-       
+                {diceElement}
+      </div>
+      <button className="roll-dice" onClick={handleClick}>Roll Dice</button>
     </main>
   );
 }
