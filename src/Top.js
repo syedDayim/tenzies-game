3363e8ce8fc27faa6b47f817800094da4339f659
Syed Dayim Shah <dayim1277@gmail.com>
2023-03-18 19:38:05 +0530

    Tenzies game

---
+++ src/Top.js
@@ -0,0 +1,10 @@
+export default function Top(){
+    return(
+        <div className="top-container">
+            <center>
+            <h1>Tenzies</h1>
+            <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            </center>
+        </div>
+    )
+}
