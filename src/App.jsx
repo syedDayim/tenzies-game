f9b6f13a8bc062a24141dd3f1c8d23f4306b6d29
syedDayim <dayim1277@gmail.com>
2024-04-29 11:47:54 +0530

    Project initialized and Cleaned

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1>Hello It Works</h1>
+    </>
+  )
+}
+
+
